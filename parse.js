4a8db42e0aae69dd92ba83ee99763728a657647a
carlos <twincarlos98@gmail.com>
2024-11-04 13:29:03 -0800

    removed logs:

--- parse.js
+++ parse.js
@@ -1,5 +1,4 @@
 export function parsePlayerLookup(text) {
-    console.log(text);
     const a = text.split('<tbody')[1].split('<td class=\"list-column\">');
     const players = [];
     for (let i = 1; i < a.length; i += 6) {
