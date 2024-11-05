e3edbaec4d1cd7e3edf17604382c39dc0b7875b1
carlos <twincarlos98@gmail.com>
2024-11-04 20:33:47 -0800

    new headers strategy did not work

--- index.js
+++ index.js
@@ -17,25 +17,10 @@ app.get('/usatt/player-lookup/:keyword', async (req, res) => {
 
   let browser;
   try {
-    browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
-        '--window-size=1920x1080',
-      ],
-    });
+    browser = await puppeteer.launch({ headless: true });
 
     const page = await browser.newPage();
 
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36');
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Referer': 'https://usatt.simplycompete.com/',
-    });
     await page.goto(url, { waitUntil: 'networkidle2' });
 
     const content = await page.content();
